3abd125ac01234152905e19416bc17779113df36
jh <sawogus29@gmail.com>
2020-11-10 20:20:53 +0900

    add tagging macro

---
+++ tools/break_p2s_pseudop.py
@@ -0,0 +1,21 @@
+import nltk
+import sys
+# nltk.download('punkt')
+
+with open(sys.argv[1], 'r', encoding='UTF-8') as fr, open(sys.argv[1] + "_sent.html", 'w', encoding='UTF-8') as fw:
+    while True:
+        p = fr.readline()
+        if not p: break
+        if len(p.strip()) == 0: continue
+
+        sents = nltk.sent_tokenize(p)
+
+        fw.write("<p>")
+        fw.write("<span class=\"pp\">\n")
+        for sent in sents:
+            fw.write("<span class=\"sent\">")
+            fw.write(sent)
+            fw.write("</span>\n")
+        fw.write("</span>")
+        fw.write("</p>\n")
+
